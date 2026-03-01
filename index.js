1edbdb253c4a8b1286177b59945d865c04242b38
NoRo2500 <98254615+NoRo2500@users.noreply.github.com>
2026-03-01 19:55:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client, GatewayIntentBits, Collection } = require("discord.js");
 const fs = require("fs");
 const path = require("path");
+const { REST, Routes } = require("discord.js");
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
@@ -22,8 +23,9 @@ for (const folder of commandFolders) {
   }
 }
 
-client.once("ready", () => {
+client.once("ready", async () => {
   console.log(`Logged in as ${client.user.tag}`);
+  await deployCommands();
 });
 
 client.on("interactionCreate", async interaction => {
@@ -39,5 +41,34 @@ client.on("interactionCreate", async interaction => {
     await interaction.reply({ content: "Error executing command.", ephemeral: true });
   }
 });
+
+async function deployCommands() {
+  const fs = require("fs");
+  const path = require("path");
+
+  const commands = [];
+  const commandsPath = path.join(__dirname, "commands");
+  const commandFolders = fs.readdirSync(commandsPath);
+
+  for (const folder of commandFolders) {
+    const folderPath = path.join(commandsPath, folder);
+    const commandFiles = fs.readdirSync(folderPath).filter(file => file.endsWith(".js"));
+
+    for (const file of commandFiles) {
+      const filePath = path.join(folderPath, file);
+      const command = require(filePath);
+      commands.push(command.data.toJSON());
+    }
+  }
+
+  const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+  await rest.put(
+    Routes.applicationCommands(process.env.CLIENT_ID),
+    { body: commands }
+  );
+
+  console.log("Slash commands deployed.");
+}
 
 client.login(process.env.TOKEN);
