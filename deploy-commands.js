95e2ae91edd079b948a7e160c675a9a025128eb7
NoRo2500 <98254615+NoRo2500@users.noreply.github.com>
2026-03-01 19:50:30 +0200

    Create deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,35 @@
+const { REST, Routes } = require("discord.js");
+const fs = require("fs");
+const path = require("path");
+
+const commands = [];
+const commandsPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(commandsPath);
+
+for (const folder of commandFolders) {
+  const folderPath = path.join(commandsPath, folder);
+  const commandFiles = fs.readdirSync(folderPath).filter(file => file.endsWith(".js"));
+
+  for (const file of commandFiles) {
+    const filePath = path.join(folderPath, file);
+    const command = require(filePath);
+    commands.push(command.data.toJSON());
+  }
+}
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log("Registering slash commands...");
+
+    await rest.put(
+      Routes.applicationCommands(process.env.CLIENT_ID),
+      { body: commands }
+    );
+
+    console.log("Slash commands registered.");
+  } catch (error) {
+    console.error(error);
+  }
+})();
